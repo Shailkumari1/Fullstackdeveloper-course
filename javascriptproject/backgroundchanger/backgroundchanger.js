8ca3e9f7fced9ecb678387e9720bf968a1c3e590
shailkumari <shailykatiyar36@gmail.com>
2026-03-05 21:15:53 +0530

    buttons background changer

---
+++ javascriptproject/backgroundchanger/backgroundchanger.js
@@ -0,0 +1,23 @@
+const red = document.getElementById('red');
+red.addEventListener('click', () =>{
+    document.body.style.backgroundColor = 'red';
+})
+
+const green = document.getElementById('green');
+green.addEventListener('click',()=>{
+    document.body.style.backgroundColor= "green";
+})
+const yellow = document.getElementById('yellow');
+yellow.addEventListener("click",() =>{
+    document.body.style.backgroundColor ="yellow";
+})
+
+const purple = document.getElementById('purple');
+purple.addEventListener("click",() =>{
+    document.body.style.backgroundColor ="purple";
+})
+
+const pink = document.getElementById('pink');
+pink.addEventListener("click",() =>{
+    document.body.style.backgroundColor ="pink";
+})
